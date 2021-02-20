c63eb1aa13227b49dd79350a1783ce87915df4a7
Stephen Ewart <screwart127@gmail.com>
2021-02-20 11:37:08 +0000

    inital push

---
+++ CarQuote/CarQuote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarQuote.Startup))]
+namespace CarQuote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
